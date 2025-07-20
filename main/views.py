3a35ec12658f9d45dfd69ffdb81751766583e339
Charmull <527wlals@gmail.com>
2025-07-20 18:54:04 +0900

    [feat]: ajax 실습 수업 코드

--- main/views.py
+++ main/views.py
@@ -30,3 +30,25 @@ def post_new(request):
             'form': form,
         }
         return render(request, 'main/post_new.html', ctx)
+    
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+
+@csrf_exempt
+def like_ajax(request):
+    req = json.loads(request.body)  # {id: 1, type: 'like'}
+    post_id = req['id'] # 1
+    button_type = req['type'] # 'like'
+
+    post = Post.objects.get(id=post_id)
+
+    if button_type == 'like':
+        post.like += 1
+    else:
+        post.dislike += 1
+
+    post.save()
+
+    return JsonResponse({'id': post_id, 'type': button_type})
